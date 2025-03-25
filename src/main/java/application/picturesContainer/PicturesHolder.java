b9307eda6610a2b142371e5bf844eb1315e440a5
2BuRy1 <kot514877@gmail.com>
2025-03-25 19:16:47 +0300

    some fixes to an application in case it falls

--- src/main/java/application/picturesContainer/PicturesHolder.java
+++ src/main/java/application/picturesContainer/PicturesHolder.java
@@ -1,6 +1,7 @@
 package application.picturesContainer;
 
 import application.services.FileService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -8,15 +9,16 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 
 @Component
 @Scope("singleton")
 public class PicturesHolder {
-
-    private final HashMap<String, String> picturesCash= new HashMap<>();
 
     private final HashMap<String, String> pictures= new HashMap<>();
 
@@ -31,17 +33,36 @@ public class PicturesHolder {
 
 
 
-
-    public void remove(String name){
-        picturesCash.remove(name);
-    }
-
     public void addToPictures(String name, String fullMultipartName){
         pictures.put(name, fullMultipartName);
     }
 
     public String getPictureByName(String name){
         return pictures.get(name);
+    }
+
+
+
+    @PostConstruct
+    public void init(){
+
+      Path path = Paths.get("uploads/");
+
+        try {
+            Files.walk(path).
+                    filter(file -> !file.getFileName().toString().equals("uploads"))
+                    .forEach(file -> {
+                        pictures.put(file.getFileName().toString().split("\\.")[0],
+                                file.getFileName().toString()
+                        );
+                    });
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 
